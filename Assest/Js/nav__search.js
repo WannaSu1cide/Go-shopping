b403808e0b22e54ab3492c9dcc93c5af60b193d4
WannaSu1cide <ngdanh1509@gmail.com>
2023-03-26 09:20:20 +0700

    new update

--- Assest/Js/nav__search.js
+++ Assest/Js/nav__search.js
@@ -1,17 +1,17 @@
 const search__icon = document.querySelector(".search__icon")
-let appear = true;
+let searching  = true;
 const navbar__input = document.querySelector("#search__input")
 const navSearch = document.querySelector(".navbar__search")
 function submit(){
-    if(appear){
+    if(searching){
         navSearch.classList.add("show__search");
         navSearch.classList.remove("close__search")
-        appear =false;
+        searching =false;
         navbar__input.focus()
     }else {
         navSearch.classList.add("close__search");
         navSearch.classList.remove("show__search")
-        appear=true;
+        searching=true;
     }
 }
 search__icon.addEventListener("click",submit)
--- Assest/Js/page.js
+++ Assest/Js/page.js
@@ -1,5 +1,27 @@
 const infoImg  =document.querySelector("#info__img");
 const infoTable =document.querySelector(".info__table")
-infoImg.addEventListener("click",()=>{
-    infoTable.classList.toggle("show__table__info")
+const body = document.querySelector(".body");
+let appear =true
+infoImg.addEventListener("click",function  openInfoTable(){
+    if(appear ===true){
+    infoTable.classList.add("show__table__info");
+    infoTable.classList.remove("close__table__info")
+    appear=false;
+    return true;
+}else {
+    infoTable.classList.add("close__table__info");
+    infoTable.classList.remove("show__table__info");
+    appear =true
+    return false
+}
+
+
+
+})
+body.addEventListener("click",()=>{
+    if(infoTable.classList.contains("show__table__info")){
+        infoTable.classList.add("close__table__info");
+        infoTable.classList.remove("show__table__info");
+        appear =true
+    }
 })
